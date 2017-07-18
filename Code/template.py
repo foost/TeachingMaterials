e239c971e394e0576a477a8900fdd900a925d489
foost <f.o.ostermann@utwente.nl>
2017-07-18 10:31:56 +0200

    added template, minor fixes

---
+++ Code/template.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Modified on Mon Jul 12 2017
+
+@author: OstermannFO
+
+template.py: 
+    provides an easy template for new scripts
+    
+some other comments can be placed here as well
+"""
+
+#
+# import statements
+#
+
+
+
+#
+#declare user variables in CAPITALS
+# 
+
+
+
+def main():
+    print "hello world"
+
+if __name__=="__main__":
+    main() 
